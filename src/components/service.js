868487d101272668c1c845c3c674247d09d788f7
Ahmed Hossam <ahmed_hossam_012@yahoo.com>
2020-08-06 15:15:48 +0200

    about section

--- src/components/service.js
+++ src/components/service.js
@@ -3,25 +3,29 @@ import lines from '../assest/img/lines.svg'
 
 export default function Service(){
     return(
-        <div id='service' className= 'mainSS'>
-            {/* <div class="title">
-                <h3>What i do</h3>
-                <img src={lines}  alt="lines"/>
-            </div> */}
+        <div id='service' className='mainSS'>
+            <div className= 'pic'/> 
             <div className='container'>
-                <div class="service">
-                    <h4>Front-end</h4>
-                    <p>As a javascript developer, I have experience in HTML5 and CSS3 techniques working with jQuery or more advanced javascript MVC frameworks such as angular</p>
-                </div>
-                <div class="service">
-                    <h4>Back-end</h4>
-                    <p>Utilising php frameworks such as Zend or creating bespoke code, I've written services supporting thousands of users, including REST APIs, e-learning applications and more.</p>
-                </div>  
+                <div class="title">
+                    <h1>What i do</h1>
+                    <img src={lines}  alt="lines"/>
+                </div> 
+                <div className='services'>
+                    <div class="service">
+                        <h4>Front-end</h4>
+                        <p>As a javascript developer, I have experience in HTML5 and CSS3 techniques working with jQuery or more advanced javascript MVC frameworks such as angular</p>
+                    </div>
+                    <div class="service">
+                        <h4>Back-end</h4>
+                        <p>Utilising php frameworks such as Zend or creating bespoke code, I've written services supporting thousands of users, including REST APIs, e-learning applications and more.</p>
+                    </div>  
                     <div class="service">
                         <h4>Consultancy</h4>
                         <p>As well as providing development services, I can also help you decide strategic roadmaps via consultancy services.</p>
+                    </div>
                 </div>
-            </div>
+            </div> 
         </div>
+        
     )
 }
